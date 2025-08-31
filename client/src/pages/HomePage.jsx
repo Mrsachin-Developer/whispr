aa57d1d6dd5e26ace379555f34c2ef26759df304
Mrsachin-Developer <tksfriday2024@gamil.com>
2025-09-01 00:09:16 +0530

    Home page is just added

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
+import ChatContainer from "../components/chatContainer";
+import RightSideBar from "../components/RightSideBar";
 
 export default function HomePage() {
-  return <div>Home</div>;
+  const [selectedUser, setSelectedUser] = useState(false);
+  return (
+    <div className="h-screen w-full border sm:px-[15%] sm:py-[5%]">
+      <div className="grid-cols-1 grid backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]  relative">
+        <SideBar />
+        <ChatContainer />
+        <RightSideBar />
+      </div>
+    </div>
+  );
 }
