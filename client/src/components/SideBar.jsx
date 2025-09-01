ef7766489b9b9c24dc38e4073ba3e1bbd440c448
Mrsachin-Developer <tksfriday2024@gamil.com>
2025-09-01 23:56:21 +0530

    right sidebar

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import assets from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-export default function SideBar() {
-  return <div>SideBar</div>;
+export default function SideBar({ selectedUser, setSelectedUser }) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-auto text-white ${
+        selectedUser ? "max-md:hidden" : ""
+      }`}
+    >
+      <div className="p-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="logo" className="max-w-40" />
+          <div className="relative py-2 group">
+            <img src={assets.menu_icon} alt="menu" className="max-h-5" />
+            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-b-gray-600 text-gray-100 hidden group-hover:block">
+              <p
+                onClick={() => navigate("/profile")}
+                className="cursor-pointer text-sm"
+              >
+                Edit Profile
+              </p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,7 +7,13 @@ export default function HomePage() {
   const [selectedUser, setSelectedUser] = useState(false);
   return (
     <div className="h-screen w-full border sm:px-[15%] sm:py-[5%]">
-      <div className="grid-cols-1 grid backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]  relative">
+      <div
+        className={`grid-cols-1 grid backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]  relative ${
+          selectedUser
+            ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "md:grid-cols-2"
+        }`}
+      >
         <SideBar />
         <ChatContainer />
         <RightSideBar />
