f8297ae2b747117ee8b732188a39b278bbca7ec5
Mrsachin-Developer <tksfriday2024@gmail.com>
2025-09-02 20:59:15 +0530

    Done nothing

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,7 +6,11 @@ import RightSideBar from "../components/RightSideBar";
 export default function HomePage() {
   const [selectedUser, setSelectedUser] = useState(false);
   return (
-    <div className="h-screen w-full border sm:px-[15%] sm:py-[5%]">
+    <div
+      className="
+     w-full h-screen sm:px-[15%] sm:py-[5%] border
+    "
+    >
       <div
         className={`grid-cols-1 grid backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%]  relative ${
           selectedUser
